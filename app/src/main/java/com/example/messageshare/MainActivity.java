0602894d5ae16c1181f5c6a25ca1ca482853c109
Mohamed Anbar <M97.anbar@gmail.com>
2018-08-27 05:09:07 +0200

    Initial commit

---
+++ app/src/main/java/com/example/messageshare/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.messageshare;
+
+import android.content.Intent;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                EditText etMsg = (TextInputEditText) findViewById(R.id.textInputMessage);
+                String msgString = etMsg.getText().toString();
+
+                Intent shareIntent = new Intent();
+                shareIntent.setAction(Intent.ACTION_SEND);
+                shareIntent.setType("text/plain");
+                shareIntent.putExtra(Intent.EXTRA_TEXT, msgString );
+                startActivity(Intent.createChooser(shareIntent, "Share via"));
+
+            }
+        });
+    }
+
+
+}
